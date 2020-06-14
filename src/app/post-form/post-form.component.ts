ca563184f243f37c2fde6b3b04ab44cc7f638fdd
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-06-13 20:17:57 -0400

    add post to server

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { NgForm } from '@angular/forms';
 
 @Component({
 	selector: 'app-post-form',
@@ -10,13 +12,22 @@ export class PostFormComponent implements OnInit {
 
 	rows: string;
 
-	constructor() { }
+	constructor(private http: HttpClient) { }
 
 	ngOnInit(): void {
 	}
 
 	numRows(): number {
 		return parseInt(this.rows);
+	}
+
+	onPost(form: NgForm) {
+		console.log(form.form.value);
+		this.http.post<{message:string}>('https://ajet-test.firebaseio.com/posts.json', {
+			message: form.form.value.message
+		}).subscribe(resData => {
+			console.log(resData);
+		});
 	}
 
 }
