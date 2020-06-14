1db8f8c8359810f54e9af8398307e2bcccdec3d7
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-06-14 00:34:00 -0400

    allow for form-data in create()

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -11,15 +11,19 @@ export class PostsService {
 	constructor(private http: HttpClient) { }
 
 	create(postData: Post) {
-		this.http.post<Post>('https://ajet-test.firebaseio.com/posts.json', {
+		this.http.post<Post>('https://bestconn-wcsu.firebaseio.com/posts.json', {
 			...postData
+		}, {
+			headers: {
+				"Content-Type": postData.file ? 'multipart/form-data' : 'text/json'
+			}
 		}).subscribe(resData => {
 			console.log(resData);
 		});
 	}
 
 	feed() {
-		return this.http.get('https://ajet-test.firebaseio.com/posts.json');
+		return this.http.get('https://bestconn-wcsu.firebaseio.com/posts.json');
 	}
 
 }
