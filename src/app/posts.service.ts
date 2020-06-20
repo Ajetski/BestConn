b8615e1356f97d7a981cd5536f1a6b1556243598
Ajetski <ajeniski4@gmail.com>
2020-06-20 10:15:48 -0400

    make apiBase a variable

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 export interface Post {
+    username: string,
     message:string,
-    file: File
+    file?: File
 }
 
 @Injectable({
@@ -14,19 +16,15 @@ export class PostsService {
 	constructor(private http: HttpClient) { }
 
 	create(postData: Post) {
-		this.http.post<Post>('https://bestconn-wcsu.firebaseio.com/posts.json', {
+		this.http.post<Post>(environment.apiBase + '/posts.json', {
 			...postData
-		}, {
-			headers: {
-				"Content-Type": postData.file ? 'multipart/form-data' : 'text/json'
-			}
 		}).subscribe(resData => {
 			console.log(resData);
 		});
 	}
 
 	feed() {
-		return this.http.get('https://bestconn-wcsu.firebaseio.com/posts.json');
+		return this.http.get(environment.apiBase + '/posts.json');
 	}
 
 }
