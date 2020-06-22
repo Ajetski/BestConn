c125b8070d05e871d9511a0ecaa50c8daca2e7ee
Ajetski <ajeniski4@gmail.com>
2020-06-21 21:26:42 -0400

    integrate login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { PostsService, Post } from '../services/posts.service';
+import { UserService } from '../services/user.service';
 
 @Component({
 	selector: 'app-home',
@@ -7,21 +9,27 @@ import { PostsService, Post } from '../services/posts.service';
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	posts: Post[] = [];
-	
-	constructor(private postsService: PostsService) {}
+    posts: Post[] = [];
+    feedErrorMessage: string;
+    
+    
+	constructor(private postsService: PostsService, public userService: UserService) {}
 
 	ngOnInit(): void {
 		this.refreshFeed();
 	}
 
 	refreshFeed(){
+        console.log("refreshing feed...");
 		this.posts = [];
 		this.postsService.feed().subscribe(posts => {
 			for(let postId in posts){
 				this.posts.push(posts[postId]);
 			}
-		});
+		}, (errData) => {
+            console.log("err", errData);
+            this.feedErrorMessage = "Error fetching feed: " + errData.message;
+        });
 	}
 
 }
