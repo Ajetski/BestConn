5bbee6cf39c5195709b15a188ba1edad4379c185
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-07-07 17:47:41 -0400

    fix firebase setup for first deployment; LIVE :)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+	firebase: {
+		apiKey: "AIzaSyDBoInx8iZZd5eoHTERXEzA7vaBjkR40Ss",
+		authDomain: "bestconn-wcsu.firebaseapp.com",
+		databaseURL: "https://bestconn-wcsu.firebaseio.com",
+		projectId: "bestconn-wcsu",
+		storageBucket: "bestconn-wcsu.appspot.com",
+		messagingSenderId: "801040037707",
+		appId: "1:801040037707:web:5adb7000b66a3bca7f5ace",
+		measurementId: "G-PC9SD34CXH"
+	}
 };
