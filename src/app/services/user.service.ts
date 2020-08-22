bc44925bf161a40380037abc4bab3e702823a7be
Ajetski <ajeniski4@gmail.com>
2020-08-21 21:36:37 -0400

    convert to use async funcitons

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,58 +10,98 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class UserService {
 
 	private _loggedIn = false;
-	private _user: User = null;
+    private _user: User = null;
+    private _isInitializing = true;
 
-	constructor(private auth: AngularFireAuth, private firestore: AngularFirestore) { }
+    constructor(private auth: AngularFireAuth, private firestore: AngularFirestore) {
+        this.init();
+    }
 
-	get loggedIn() {
+    get loggedIn() {
 		return this._loggedIn;
 	}
 	
 	get user(): User {
 		return this._user;
+    }
+    
+    get isInitializing() {
+        return this._isInitializing;
+    }
+    
+    // constructor cannot be async, so use separate method
+    async init() {
+        try {
+            await this.auth.setPersistence('local');
+            this._user = JSON.parse(localStorage.getItem('user'));
+            const test = await this.firestore.collection('/test').doc('testDoc').get().toPromise();
+            console.log(test);
+            this._loggedIn = true;
+            console.log("logged in as: ", this._user);
+        }
+        catch(err) {
+            this.signOut();
+        }
+        this._isInitializing = false;
+    }
+
+	async login(email: string, password: string) {
+        try {            
+            const userCreds = await this.auth.signInWithEmailAndPassword(email, password);
+                
+            this.firestore.collection('/users', ref => ref.where('uid', '==', userCreds.user.uid)).get().forEach(userDocs => {
+                console.log(userDocs);
+                userDocs.docs.forEach(userDoc => {
+                    const user = userDoc.data();
+                    this._user = {
+                        uid: user.uid,
+                        username: user.username,
+                        email: user.email,
+                        bio: user.bio,
+                        following: user.following,
+                        followers: user.followers
+                    } as User
+                });
+                localStorage.setItem('user', JSON.stringify(this._user));
+                console.log('Logged In as: ', this.user);
+                this._loggedIn = true;
+            });
+        }
+        catch (err) {
+            this.error(err);
+	    }
 	}
 
-	login(email: string, password: string) {
-		console.log("sign in data: ", email, password);
-		this.auth.signInWithEmailAndPassword(email, password).then(() => {
-			this.auth.currentUser.then((authUser) => {
-				this.firestore.collection('/users', ref => ref.where('uid', '==', authUser.uid)).get().forEach(userDocs => {
-					userDocs.docs.forEach(userDoc => {
-						const user = userDoc.data();
-						this._user = {
-							uid: user.uid,
-							username: user.username,
-							email: user.email,
-							bio: user.bio,
-							following: user.following,
-							followers: user.followers
-						} as User
-					})
-					console.log('Logged In as: ', this.user);
-					this._loggedIn = true;
-				})
-			}).catch(err => this.error(err));
-		}).catch((err) => {
-			this.error(err);
-		});
-	}
-
-	signUp(user: User, password: string) {
-		this.auth.createUserWithEmailAndPassword(user.email, password).then(() => {
+	async signUp(user: User, password: string) {
+        try {
+		    await this.auth.createUserWithEmailAndPassword(user.email, password);
 			this._loggedIn = true;
-			this.auth.currentUser.then(newUser => {
-				user.uid = newUser.uid;
-				this.firestore.collection('users').add(user).then(() => {
-					console.log("Signed up and logged in.");
-					this._user = user;
-				}).catch(err => this.error(err));
-			})
-		}).catch((err) => this.error(err));
-	}
+			const newUser = await this.auth.currentUser;
+            user.uid = newUser.uid;
+            await this.firestore.collection('users').add(user);
+            this._user = user;
+            localStorage.setItem('user', JSON.stringify(this._user));
+            console.log("Signed up and logged in as: ", this._user);
+        }
+        catch (err) {
+            this.error(err);
+        }
+    }
+
+    async signOut() {
+        localStorage.removeItem('user');
+        try {
+            this.auth.signOut();
+        }
+        catch (err) {
+            console.log("Error signing out: ", err);
+        }
+        this._loggedIn = false;
+    }
+
 
 	private error(err) {
-		console.error("Error Signing up or logging in: ", err);
-		this._loggedIn = false;
+        console.error("Error Signing up or logging in: ", err);
+        this.signOut();
 	}
 }
