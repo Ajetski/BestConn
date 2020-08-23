b6b158c05becad0ae1b73512900201003b41277f
Adam M Jensiki <adam.jeniski@ibm.com>
2020-08-23 12:28:09 -0400

    auto-loggout on exception

--- src/app/error-handler.ts
+++ src/app/error-handler.ts
@@ -1,16 +1,20 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import { LoggingService } from './services/logging.service';
+import { UserService } from './services/user.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-	constructor(private logger: LoggingService) { }
+	constructor(private logger: LoggingService, private userService: UserService) { }
 
 	handleError(error: Error) {
         // pick either the JSON or error.message
         const errMessage = JSON.stringify(error) === "{}" ? error.message : JSON.stringify(error);
         
         // log error to discord
-        this.logger.log("Error: ```" + errMessage  + "```");
+        this.logger.log("Error: ```" + errMessage  + "```User: ```" + localStorage.getItem('user') + "```");
+
+        // sign out to avoid repeat issues. this resets login state for the user
+        this.userService.signOut();
 
         // prevent error from being burried
 		throw error;
